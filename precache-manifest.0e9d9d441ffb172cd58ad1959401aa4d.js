380212319bf2d20ccaaf8ec69a880d41876eb470
Jennifer <jen.yiu.90@gmail.com>
2019-04-29 00:37:13 -0700

    Updates

---
+++ precache-manifest.0e9d9d441ffb172cd58ad1959401aa4d.js
@@ -0,0 +1,78 @@
+self.__precacheManifest = [
+  {
+    "revision": "f87f718a9e5b4d65b06d",
+    "url": "/portfolio/static/css/main.9d5312b6.chunk.css"
+  },
+  {
+    "revision": "f87f718a9e5b4d65b06d",
+    "url": "/portfolio/static/js/main.d3c2d74f.chunk.js"
+  },
+  {
+    "revision": "53ebc025fca807d4dbea",
+    "url": "/portfolio/static/js/runtime~main.f308c8e6.js"
+  },
+  {
+    "revision": "c289a283c166f2e99a0e",
+    "url": "/portfolio/static/js/2.b57cf874.chunk.js"
+  },
+  {
+    "revision": "26b690e5feb7abb101ef535515730618",
+    "url": "/portfolio/static/media/BoomItems.26b690e5.png"
+  },
+  {
+    "revision": "44c026105596ce99d90f38ac19e0db04",
+    "url": "/portfolio/static/media/mainstbrewing.44c02610.gif"
+  },
+  {
+    "revision": "054b20b24ba34d3d1e44ef54a32d3a75",
+    "url": "/portfolio/static/media/FBOnboardGiphy.054b20b2.gif"
+  },
+  {
+    "revision": "34c24e85df6eb7be0fa6e8a344590030",
+    "url": "/portfolio/static/media/R10.34c24e85.png"
+  },
+  {
+    "revision": "cb50682b450fe881d70f51e6ca06a76b",
+    "url": "/portfolio/static/media/calmversation.cb50682b.png"
+  },
+  {
+    "revision": "e3b108bb14cff42ffdda13d821267caf",
+    "url": "/portfolio/static/media/BoomHome.e3b108bb.png"
+  },
+  {
+    "revision": "f119b528de3b5bf88b89953a3d9065da",
+    "url": "/portfolio/static/media/BoomShare.f119b528.png"
+  },
+  {
+    "revision": "30a1532c7dc3e2229d681fe76f156635",
+    "url": "/portfolio/static/media/calm.30a1532c.png"
+  },
+  {
+    "revision": "1c4e1e493edb73b44ce79b6d3eec3a23",
+    "url": "/portfolio/static/media/redbg-small.1c4e1e49.jpeg"
+  },
+  {
+    "revision": "4baebcea8da4490d74d9423b0a77d9d7",
+    "url": "/portfolio/static/media/redbg-mobile.4baebcea.png"
+  },
+  {
+    "revision": "87da8261339cebdeefca037452f9c052",
+    "url": "/portfolio/static/media/gh.87da8261.svg"
+  },
+  {
+    "revision": "39a796eca1456d7b471e8c500da661c4",
+    "url": "/portfolio/static/media/vietnam-motorbike.39a796ec.jpg"
+  },
+  {
+    "revision": "e82a19fafb32f70e7c1ced6b00ee5f04",
+    "url": "/portfolio/static/media/bangkok-mural.e82a19fa.jpg"
+  },
+  {
+    "revision": "527db0ba344ddb59b346cca37d76458f",
+    "url": "/portfolio/static/media/nicomos-lick.527db0ba.jpg"
+  },
+  {
+    "revision": "5e402fc62005b87e357de4b689a068e8",
+    "url": "/portfolio/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/portfolio/precache-manifest.2936f273b66834be3b5f7efc7b0a18b2.js"
+  "/portfolio/precache-manifest.0e9d9d441ffb172cd58ad1959401aa4d.js"
 );
 
 workbox.clientsClaim();
