702c7bf673f5c01b7c6e1ca93085e4686675744a
Jennifer <jen.yiu.90@gmail.com>
2019-06-17 19:08:05 -0700

    Updates

---
+++ precache-manifest.8cb8bb1804bb5260735bcc3831df6736.js
@@ -0,0 +1,86 @@
+self.__precacheManifest = [
+  {
+    "revision": "45f1fa52cdd823b04a7c",
+    "url": "/static/css/main.458d5709.chunk.css"
+  },
+  {
+    "revision": "45f1fa52cdd823b04a7c",
+    "url": "/static/js/main.8568ea9b.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "dab88598c63af01efe99",
+    "url": "/static/js/2.bc09f2b4.chunk.js"
+  },
+  {
+    "revision": "995ac617df7dee3baed546715d7e446a",
+    "url": "/static/media/GreyMob.995ac617.png"
+  },
+  {
+    "revision": "68411dab2f38cbab430874d00309240f",
+    "url": "/static/media/GreyDesk.68411dab.png"
+  },
+  {
+    "revision": "87da8261339cebdeefca037452f9c052",
+    "url": "/static/media/gh.87da8261.svg"
+  },
+  {
+    "revision": "4baebcea8da4490d74d9423b0a77d9d7",
+    "url": "/static/media/redbg-mobile.4baebcea.png"
+  },
+  {
+    "revision": "e3b108bb14cff42ffdda13d821267caf",
+    "url": "/static/media/BoomHome.e3b108bb.png"
+  },
+  {
+    "revision": "26b690e5feb7abb101ef535515730618",
+    "url": "/static/media/BoomItems.26b690e5.png"
+  },
+  {
+    "revision": "f119b528de3b5bf88b89953a3d9065da",
+    "url": "/static/media/BoomShare.f119b528.png"
+  },
+  {
+    "revision": "44c026105596ce99d90f38ac19e0db04",
+    "url": "/static/media/mainstbrewing.44c02610.gif"
+  },
+  {
+    "revision": "054b20b24ba34d3d1e44ef54a32d3a75",
+    "url": "/static/media/FBOnboardGiphy.054b20b2.gif"
+  },
+  {
+    "revision": "34c24e85df6eb7be0fa6e8a344590030",
+    "url": "/static/media/R10.34c24e85.png"
+  },
+  {
+    "revision": "cb50682b450fe881d70f51e6ca06a76b",
+    "url": "/static/media/calmversation.cb50682b.png"
+  },
+  {
+    "revision": "a95fc6540f470b656dc4fbd8a5f49a36",
+    "url": "/static/media/hack4humanity.a95fc654.png"
+  },
+  {
+    "revision": "39a796eca1456d7b471e8c500da661c4",
+    "url": "/static/media/vietnam-motorbike.39a796ec.jpg"
+  },
+  {
+    "revision": "e82a19fafb32f70e7c1ced6b00ee5f04",
+    "url": "/static/media/bangkok-mural.e82a19fa.jpg"
+  },
+  {
+    "revision": "16bb41e1cea895295dafe05ecd32862b",
+    "url": "/static/media/RedAcademyYVR_Headshots_Jennifer_20181207_040.16bb41e1.jpg"
+  },
+  {
+    "revision": "e095bdb707b386cc36165fba50c6b8b1",
+    "url": "/static/media/scubadive.e095bdb7.png"
+  },
+  {
+    "revision": "c06796ffa88a395f12978d074aabc14e",
+    "url": "/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/portfolio/precache-manifest.6828965f6750d8574d0edd65e1ffb471.js"
+  "/precache-manifest.8cb8bb1804bb5260735bcc3831df6736.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/portfolio/index.html", {
+workbox.routing.registerNavigationRoute("/index.html", {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
